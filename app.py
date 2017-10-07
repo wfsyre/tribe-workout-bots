8e4a8b10f70cf13772d72f32faad5d4249f22647
William Syre <wfsyre@gmail.com>
2017-10-07 14:22:53 +0100

    Tried to fix endless loop

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def webhook():
     log('Recieved {}'.format(data))
 
     # We don't want to reply to ourselves!
-    if data['name'] != 'tribe-2017-2018-groupmebot':
+    if data['name'] != 'Woo':
         msg = '{}, you sent "{}".'.format(data['name'], data['text'])
         send_message(msg)
 
