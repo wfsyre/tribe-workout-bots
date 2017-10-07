f21c1081f7c73b88b441301f52ef2cce2591446f
William Syre <wfsyre@gmail.com>
2017-10-07 14:09:24 +0100

    Changed name to actual bot name

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 
 from urllib.parse import urlencode
@@ -7,36 +8,32 @@ from urllib.request import Request, urlopen
 from flask import Flask, request
 
 app = Flask(__name__)
+
+
 @app.route('/', methods=['POST'])
 def webhook():
-  data = request.get_json()
+    data = request.get_json()
+    log('Recieved {}'.format(data))
 
-  # We don't want to reply to ourselves!
-  if data['name'] != 'tribe-2017-2018-groupmebot':
-    msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-    send_message(msg)
+    # We don't want to reply to ourselves!
+    if data['name'] != 'apnorton-test-bot':
+        msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+        send_message(msg)
 
-  return "ok", 200
-{
-  "attachments": [],
-  "avatar_url": "http://i.groupme.com/123456789",
-  "created_at": 1302623328,
-  "group_id": "1234567890",
-  "id": "1234567890",
-  "name": "John",
-  "sender_id": "12345",
-  "sender_type": "user",
-  "source_guid": "GUID",
-  "system": false,
-  "text": "Hello world ☃☃",
-  "user_id": "1234567890"
-}
+    return "ok", 200
+
+
 def send_message(msg):
-  url  = 'https://api.groupme.com/v3/bots/post'
+    url = 'https://api.groupme.com/v3/bots/post'
 
-  data = {
-          'bot_id' : os.getenv('GROUPME_BOT_ID'),
-          'text'   : msg,
-         }
-  request = Request(url, urlencode(data).encode())
-  json = urlopen(request).read().decode()
+    data = {
+        'bot_id': os.getenv('GROUPME_BOT_ID'),
+        'text': msg,
+    }
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
+
+
+def log(msg):
+    print(str(msg))
+    sys.stdout.flush()
