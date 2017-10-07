b5e491d98ed6967b4404273d9f85526978ffb7cd
William Syre <wfsyre@gmail.com>
2017-10-07 15:15:49 +0100

    discovering what an attachment is v6

--- app.py
+++ app.py
@@ -15,20 +15,20 @@ def webhook():
     data = request.get_json()
     log('Recieved {}'.format(data))
 
-    group_members = get_group_info(data['group_id'])
     # We don't want to reply to ourselves!
     if data['name'] != 'TEST':
         msg = '{}, you sent "{}".'.format(data['name'], data['text'])
         send_message(msg)
-    if len(data['attachments']) > 0:
-        names = []
-        for attachment in data["attachments"]:
-            if attachment['type'] == 'mentions':
-                for user in attachment['user_ids']:
-                    for member in group_members["members"]:
-                        if user == member["user_id"]:
-                            names.append(member["nickname"])
-        send_message(str(names))
+        if len(data['attachments']) > 0:
+            group_members = get_group_info(data['group_id'])
+            names = []
+            for attachment in data["attachments"]:
+                if attachment['type'] == 'mentions':
+                    for user in attachment['user_ids']:
+                        for member in group_members["members"]:
+                            if user == member["user_id"]:
+                                names.append(member["nickname"])
+            send_message(str(names))
     return "ok", 200
 
 
@@ -48,8 +48,7 @@ def log(msg):
     sys.stdout.flush()
 
 def get_group_info(group_id):
-    send_message("1")
-    url = "https://api.groupme.com/v3/groups/%d?token=%s"% group_id, os.getenv("ACCESS_TOKEN")
+    url = "https://api.groupme.com/v3/groups/%s?token=%s" % group_id, os.getenv("ACCESS_TOKEN")
     send_message("2")
     request = Request(url)
     send_message("3")
