409966c1ecbf6d4f876c56d62761c5dc8547e5fd
William Syre <wfsyre@gmail.com>
2017-12-07 20:58:55 +0000

    added support for id based verification

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def webhook():
                 "UPDATE tribe_data SET num_posts = num_posts+1, id = %s WHERE name = %s"),
                 (data['id'], data['name'],))
             if cursor.rowcount == 0:
-                cursor.execute(sql.SQL("INSERT INTO tribe_data VALUES (%s, 1, 0, 0, now()), %s"), (data['name'], data['user_id']))
+                cursor.execute(sql.SQL("INSERT INTO tribe_data VALUES (%s, 1, 0, 0, now()), %s"), (data['name'], data['id']))
                 send_debug_message("added %s to the group" % data['name'])
             conn.commit()
             cursor.close()
@@ -76,6 +76,7 @@ def webhook():
                                 if member["user_id"] == mentioned:
                                     names.append(member["nickname"])
                                     ids.append(member["user_id"])
+                                    send_debug_message(str(member["user_id"]))
                 if found_attachment: #append the poster to the list of names to be uodated in the database
                     names.append(data['name'])
                     ids.append(data['user_id'])
@@ -203,10 +204,11 @@ def add_to_db(names, addition, ids): #add "addition" to each of the "names" in t
             cursor.execute(sql.SQL(
                 "UPDATE tribe_data SET num_workouts = num_workouts+1, workout_score = workout_score+%s, last_post = now() WHERE id = %s"),
                 [str(addition), ids[x]])
-
-            #if cursor.rowcount == 0: #Shouldn;t be necessary
-            #    cursor.execute(sql.SQL("INSERT INTO tribe_data VALUES(%s, %s, %s, %s"), (names[x], "0", "1", str(addition), ids[x]))
-            #    send_debug_message("added %s to the group" % names[x])
+            if cursor.rowcount == 0: #If a user does not have an id yet
+                cursor.execute(sql.SQL(
+                    "UPDATE tribe_data SET num_workouts = num_workouts+1, workout_score = workout_score+%s, last_post = now() WHERE name = %s"),
+                    [str(addition), names[x]])
+                send_debug_message("%s does not have an id yet" % names[x])
             conn.commit()
             send_debug_message("committed %s" % names[x])
     except (Exception, psycopg2.DatabaseError) as error:
