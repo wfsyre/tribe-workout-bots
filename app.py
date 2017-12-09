7c99cf373e68f869b17ef2dc3042c51c12cb5ffa
William Syre <wfsyre@gmail.com>
2017-12-09 12:38:09 +0000

    changed id to user_id globally

--- app.py
+++ app.py
@@ -35,9 +35,9 @@ def webhook():
             #add 1 to the number of posts of the person that posted
             cursor.execute(sql.SQL(
                 "UPDATE tribe_data SET num_posts = num_posts+1, id = %s WHERE name = %s"),
-                (data['id'], data['name'],))
+                (data['user_id'], data['name'],))
             if cursor.rowcount == 0:
-                cursor.execute(sql.SQL("INSERT INTO tribe_data VALUES (%s, 1, 0, 0, now()), %s"), (data['name'], data['id']))
+                cursor.execute(sql.SQL("INSERT INTO tribe_data VALUES (%s, 1, 0, 0, now()), %s"), (data['name'], data['user_id']))
                 send_debug_message("added %s to the group" % data['name'])
             conn.commit()
             cursor.close()
