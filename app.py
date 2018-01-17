b4e2734b42b65bbe623155107ffe6481784503cf
William Syre <wsyre3@gatech.edu>
2018-01-17 00:04:12 -0500

    SQL syntax

--- app.py
+++ app.py
@@ -318,7 +318,7 @@ def add_hydration(data, addition):
         cursor = conn.cursor()
         for x in range(0, len(names)):
             cursor.execute(sql.SQL(
-                "UPDATE tribe_water SET num_liters = num_liters+%s, WHERE id = %s"),
+                "UPDATE tribe_water SET num_liters = num_liters+%s WHERE id = %s"),
                 (str(addition), ids[x],))
             if cursor.rowcount == 0:  # If a user does not have an id yet
                 cursor.execute(sql.SQL(
