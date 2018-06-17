875e37a8aa887754a35f0a5d9da9ddc65e38d407
William Syre <wfsyre@gmail.com>
2018-06-17 10:05:57 -0400

    perfecting !regionals

--- app.py
+++ app.py
@@ -448,3 +448,12 @@ def send_direct_message(user_id, text):
 def stringFromSeconds(seconds):
     if seconds < 0:
         return seconds, " seconds. You missed it, better luck next year."
+    else:
+        days = seconds / 60 / 60 / 24
+        fracDays = days - int(days)
+        hours = fracDays * 24
+        fracHours = hours - int(hours)
+        minutes = fracHours * 60
+        fracMinutes = minutes - int(minutes)
+        seconds = fracMinutes * 60
+        return "%d days, %d hours, %d minutes, %d seconds" % (days, minutes, hours, seconds)
