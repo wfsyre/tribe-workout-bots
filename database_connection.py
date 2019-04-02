375e4c8f4cbf556b22dab97211013d6f9683fc34
wfsyre <wfsyre@gmail.com>
2019-04-02 11:53:13 -0400

    adding groundwork for new attendance bot

--- database_connection.py
+++ database_connection.py
@@ -326,7 +326,7 @@ def add_dummy_responses(date):
     print(date)
     member_and_id = []
     for member in get_group_info()['members']:
-        if 'bot_id' not in member:
+        if 'bot_id' not in member['profile']:
             print(member)
             member_and_id.append((member['id'], member['real_name']))
     try:
