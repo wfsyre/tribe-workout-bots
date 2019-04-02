f537569b5daab750bd1a4c6ce88a58a6c85472d8
wfsyre <wfsyre@gmail.com>
2019-04-02 11:28:39 -0400

    adding groundwork for new attendance bot

--- database_connection.py
+++ database_connection.py
@@ -340,7 +340,7 @@ def add_dummy_responses(date):
         cursor = conn.cursor()
         for slack_id, real_name in member_and_id:
             cursor.execute(sql.SQL("INSERT INTO tribe_attendance VALUES(%s, %s, -1, %s, now())"),
-                           [real_name, slack_id, str(date.strftime("%Y-%m-%d"))])
+                           [real_name, slack_id, date.strftime("%Y-%m-%d")])
         conn.commit()
         cursor.close()
         conn.close()
