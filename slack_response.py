1d4c504bae20972ef71ac3ab1be9445b254bbff8
William Syre <wsyre3@gatech.edu>
2020-01-11 17:43:49 -0500

    allowing slackbot to execute commands

--- slack_response.py
+++ slack_response.py
@@ -390,11 +390,13 @@ class SlackResponse:
 
     def execute_commands(self):
         count = 0
+        send_debug_message("Executing commands for " + self._lower_text)
         for command in self._COMMANDS:
             index = command.find("command_") + 8
             if ("!" + command[index:]) in self._lower_text and 'admin' not in command:
                 if command in dir(self):
                     # calls a method with the name scheme command_nameofcommand()
+                    send_debug_message(command)
                     getattr(self, command)()
                     count += 1
             elif ("!" + command[index:]) in self._lower_text:
